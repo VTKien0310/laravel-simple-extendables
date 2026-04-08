ccf925512f220fe61dd1c09593afcbf703a6a67a
trgkien.vu <trgkien.vu@gmail.com>
2026-04-08 10:48:44 +0700

    doc: Clarify usage of Ulid and Uuid traits for older Laravel versions
    - Add comments in `UlidEloquent.php` and `UuidEloquent.php` to guide usage for older Laravel versions.
    - Recommend using Laravel's built-in `HasUlids` and `HasUuids` traits for newer versions.

--- Extendables/Core/Models/Traits/UlidEloquent.php
+++ Extendables/Core/Models/Traits/UlidEloquent.php
@@ -5,6 +5,10 @@ namespace App\Extendables\Core\Models\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
+/**
+ * This trait should only be used in older Laravel versions.
+ * For newer versions, consider using Laravel's built-in HasUlids trait.
+ */
 trait UlidEloquent
 {
     /**
